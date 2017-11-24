796452778f4aacc03ff3c8f3b761830ceba41daa
nathcarletti <ncsoares2@gmail.com>
2017-11-24 14:28:52 -0200

    Att

--- routes/index.js
+++ routes/index.js
@@ -7,25 +7,5 @@ router.get('/', function(req, res, next){
     res.render('index.html');
 });
 
-//Can edit only this user
-function btnLogin(){
-    var name = document.getElementById('nameL').value;
-    var pass = document.getElementById('passL').value;
-    db.users.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err,user){
-        if((name==user.name) && (pass == user.password)){
-            res.json(user);
-        }
-    });
-};
-/*
-function btnEdit(){
-    db.users.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err,user){
-        if(user.name!=name){
-            res.send(err);
-        }else{
-            window.location.href = "http://localhost:3000/api/userEdit/";
-        }
-    });
-}*/
 
 module.exports = router;
